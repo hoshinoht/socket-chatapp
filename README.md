7e255f36a844c6e248258502091db6c74de55aa1
N1GO648 <nicocaleblim@gmail.com>
2025-03-14 15:10:31 +0800

    gpt gave me this. additional feature is message history

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # websocket-chatapp
+<!-- #How to Run
+	1.	Save each code block to separate files (for example, standard_server.py and standard_client.py for the standard version; similarly, enhanced_server.py and enhanced_client.py for the enhanced version).
+	2.	Start the server:
+	•	For standard:
+python standard_server.py <IP_address> <port>
+	•	For enhanced:
+python enhanced_server.py <IP_address> <port>
+	3.	Run one or more client instances similarly:
+python standard_client.py <IP_address> <port>
+or
+python enhanced_client.py <IP_address> <port>
+
+Use the commands (such as @names, @group set Friends Bob,Charlie, @group send Friends Hello everyone, @quit, and in the enhanced version, @history 10) to interact with the server. -->
--- client/client.py
+++ client/client.py
@@ -1,32 +1,47 @@
+import socket
+import select
+import sys
 
-import socket 
-import select 
-import sys 
- 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-if len(sys.argv) != 3: 
-    print ("Correct usage: script, IP address, port number")
-    exit() 
-IP_address = str(sys.argv[1]) 
-Port = int(sys.argv[2]) 
-server.connect((IP_address, Port)) 
- 
-while True: 
- 
-    # maintains a list of possible input streams 
-    sockets_list = [sys.stdin, server] 
-    read_sockets,write_socket, error_socket = select.select(sockets_list,[],[]) 
- 
-    for socks in read_sockets: 
-        if socks == server: 
-            message = socks.recv(2048) 
-            print (message) 
-        else: 
-            message = sys.stdin.readline() 
-            server.send(message) 
-            sys.stdout.write("<You>") 
-            sys.stdout.write(message) 
-            sys.stdout.flush() 
-            
-            
-server.close() 
+if len(sys.argv) != 3:
+    print("Usage: script IP_address port")
+    sys.exit()
+IP_address = str(sys.argv[1])
+Port = int(sys.argv[2])
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.connect((IP_address, Port))
+
+# --- Authentication Sequence ---
+sys.stdout.write(server.recv(2048).decode())
+sys.stdout.flush()
+username = sys.stdin.readline()
+server.send(username.encode())
+
+sys.stdout.write(server.recv(2048).decode())
+sys.stdout.flush()
+password = sys.stdin.readline()
+server.send(password.encode())
+
+response = server.recv(2048).decode()
+if response.startswith("ERROR"):
+    print(response)
+    server.close()
+    sys.exit()
+else:
+    print(response)
+
+# --- Main Chat Loop ---
+while True:
+    sockets_list = [sys.stdin, server]
+    read_sockets, _, _ = select.select(sockets_list, [], [])
+    for sock in read_sockets:
+        if sock == server:
+            message = sock.recv(2048).decode()
+            if not message:
+                print("Connection closed by server")
+                sys.exit()
+            print(message)
+        else:
+            message = sys.stdin.readline()
+            server.send(message.encode())
+server.close()
